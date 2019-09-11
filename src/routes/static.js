05776599ee3ec23e7e8c909573ef4442bbda788c
Allison Jin <ajxiaoxi@gmail.com>
2019-09-11 16:15:27 -0700

    static page done

--- src/routes/static.js
+++ src/routes/static.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  res.send("Welcome to e-invitation");
-});
+const staticController = require("../controllers/staticController");
 
+router.get("/", staticController.index);
 module.exports = router;
