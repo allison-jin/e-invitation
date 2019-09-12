6ea031ad9de94f48ab9fef25b3f1be47b333f8a9
Allison Jin <ajxiaoxi@gmail.com>
2019-09-11 20:40:25 -0700

    5-validations:event and list

--- src/routes/lists.js
+++ src/routes/lists.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const listController = require("../controllers/listController")
+const validation = require("./validation");
 
 router.get("/events/:eventId/lists/new", listController.new);
-router.post("/events/:eventId/lists/create", listController.create);
+router.post("/events/:eventId/lists/create", validation.validateLists, listController.create);
 router.get("/events/:eventId/lists/:id", listController.show);
 router.post("/events/:eventId/lists/:id/destroy", listController.destroy);
 router.get("/events/:eventId/lists/:id/edit", listController.edit);
-router.post("/events/:eventId/lists/:id/update", listController.update);
+router.post("/events/:eventId/lists/:id/update", validation.validateLists, listController.update);
 
 module.exports = router;
